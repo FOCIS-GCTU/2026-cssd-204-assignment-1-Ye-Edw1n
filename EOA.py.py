63df62eb90220a45a8409c0d85c80a24e1cfd829
Ye-Edw1n <2425401458@live.gctu.edu.gh>
2026-05-29 01:17:26 +0000

    Add files via upload
    Assignment 1

---
+++ EOA.py.py
@@ -0,0 +1,44 @@
+# FILE: EOA.py
+# DESCIPTION: EDWIN OWUSU AMPRATWUM
+# ASSIGNMENT NUMBER: 1
+#
+# NAME: EDWIN AMPRATWUM
+# STUDENT ID: 2425401458
+# EMAIL: 2425401458@live.gctu.edu.gh
+# GRADER: AUGUSTUS
+#
+# On my honor, EDWIN AMPRATWUM, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+print()
+
+def main():
+    # Prints initials E.O.A in small format and large stylized block letters.
+    # Each large letter is 12 characters wide by 10 characters high,
+    # made of the corresponding capital letter and periods as fill.
+    # A middle initial 'O' is used as a standin (for Edwin Owusu Ampratwum).
+
+
+    print()
+
+    print("...EOA")
+
+    print()
+
+    print("...EEEEEEEEEEEE........OOOOOOOOOOOO........AAAAAAAAAAAA.....")
+    print("...EEEEEEEEEEEE........OOOOOOOOOOOO........AAAAAAAAAAAA.....")
+    print("...EEEE................OOOO....OOOO........AAAA....AAAA.....")
+    print("...EEEE................OOOO....OOOO........AAAA....AAAA.....")
+    print("...EEEEEEEEEEEE........OOOO....OOOO........AAAAAAAAAAAA.....")
+    print("...EEEEEEEEEEEE........OOOO....OOOO........AAAAAAAAAAAA.....")
+    print("...EEEE................OOOO....OOOO........AAAA....AAAA.....")
+    print("...EEEE................OOOO....OOOO........AAAA....AAAA.....")
+    print("...EEEEEEEEEEEE...**...OOOOOOOOOOOO...**...AAAA....AAAA...**")
+    print("...EEEEEEEEEEEE...**...OOOOOOOOOOOO...**...AAAA....AAAA...**")
+
+    print()
+
+
+main()
+
+print()
